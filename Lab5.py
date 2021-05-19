f48d47d15322a7f7c86083120a430631269ab0e5
Vladimir Istomin <56888502+JamesBomzh@users.noreply.github.com>
2021-05-19 21:39:45 +0300

    fix

--- Lab5.py
+++ Lab5.py
@@ -5,10 +5,10 @@ import math
 
 x, y = sp.symbols('x, y')
 
-J = x ** 3 + 8*y**3 - 6*x*y + 5
+J = 2*x**3 + y**3 - x*y - 5
 
-x0 = 3
-y0 = 0.5
+x0 = 0.3
+y0 = 0.3
 alpha = 1
 
 eps = 0.0001
@@ -18,9 +18,11 @@ while math.sqrt((J.diff(x) ** 2 + J.diff(y) ** 2).subs(x, x0).subs(y, y0)) >= ep
     y1 = y0 - alpha * J.diff(y).subs(y, y0).subs(x, x0)
     J1 = J.subs(x, x1).subs(y, y1)
     J0 = J.subs(x, x0).subs(y, y0)
+    print(J0)
     if J.subs(x, x1).subs(y, y1) < J.subs(x, x0).subs(y, y0):
         x0 = x1
         y0 = y1
+        print(x0, y0)
     else:
         alpha /= 2
 
--- Lab7.py
+++ Lab7.py
@@ -17,9 +17,9 @@ y0 = 1
 
 
 while math.sqrt((P.diff(x) ** 2 + P.diff(y) ** 2).subs(x, x0).subs(y, y0)) >= eps:
-    h = 1.0 / (P.diff(x, x) * P.diff(y, y) - P.diff(x, y) * P.diff(y, x)).subs(x, x0).subs(y, y0)
-    x0 = x0 - h * P.diff(x).subs(x, x0).subs(y, y0)
-    y0 = y0 - h * P.diff(y).subs(x, x0).subs(y, y0)
+    lambd = 1.0 / (P.diff(x, x) * P.diff(y, y) - P.diff(x, y) * P.diff(y, x)).subs(x, x0).subs(y, y0)
+    x0 = x0 - lambd * P.diff(x).subs(x, x0).subs(y, y0)
+    y0 = y0 - lambd * P.diff(y).subs(x, x0).subs(y, y0)
     k += 1
     r /= 10
     P = J + r * g
